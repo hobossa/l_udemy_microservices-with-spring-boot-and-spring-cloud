4ae34e47c4ba48082b8efe2a226f4deec43c61d2
Hao Zhan <hoboss@gmail.com>
2025-03-24 01:27:06 -0700

    add a path variable GetMapping

--- restful-web-services/src/main/java/com/hoboss/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/hoboss/restful_web_services/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.hoboss.restful_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 // REST API
@@ -15,5 +16,14 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    // Path Parameters
+    // /users/{user_id}/todos/{todo_id} =>/users/2/todos/200
+    // /hello-world/path-variable/{name}
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
